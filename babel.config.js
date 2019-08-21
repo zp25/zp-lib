9dae903120c7ef9063350f7325a4323010d26ac8
zp25 <zebrap25@gmail.com>
2019-08-21 16:36:31 +0800

    build(babel): dist不添加polyfill

--- babel.config.js
+++ babel.config.js
@@ -8,21 +8,17 @@ module.exports = (api) => {
       '@babel/preset-env',
       {
         modules: false,
-        useBuiltIns: 'usage',
-        corejs: 3,
+        useBuiltIns: false,
       },
     ]);
   }
 
   const plugins = [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-json-strings',
     [
       '@babel/plugin-proposal-decorators',
       {
-        legacy: true,
+        decoratorsBeforeExport: false,
       },
     ],
     '@babel/plugin-proposal-export-namespace-from',
